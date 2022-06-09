c12f720d084299e65c8a0138634f6df278c78494
Majelvey <mckelvey.andy@gmail.com>
2022-06-09 18:39:16 -0400

    added app.get for /api/notes to show contents of db.json

--- server.js
+++ server.js
@@ -18,12 +18,36 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// Wildcard route
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for tips`);
+  readFromFile("/db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a tip`);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    readAndAppend(newNote, '/db/db.json');
+    res.json('Note added successfully');
+  } else {
+    res.error('Error in adding tip');
+  }
+});
+
+// Wildcard route that points to homepage
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// PORT Listener
+// PORT listener
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
